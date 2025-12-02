031d3737ed830e62b6e2c993a211758ebc4304a7
bidjei <bidjei@fedler.ca>
2025-12-02 09:39:24 -0800

    add packages `neogit` and `diffvie`

--- init.lua
+++ init.lua
@@ -8,18 +8,22 @@ vim.api.nvim_create_autocmd("PackChanged", { callback = function(ev)
 end})
 
 vim.pack.add {
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/nvim-mini/mini.icons",
+
 	"https://github.com/nvim-neotest/nvim-nio.git",
 	"https://github.com/mfussenegger/nvim-dap.git",
 	"https://github.com/rcarriga/nvim-dap-ui.git",
-	"https://github.com/nvim-lua/plenary.nvim",
+
+	"https://github.com/sindrets/diffview.nvim.git",
+	"https://github.com/NeogitOrg/neogit.git",
+
 	"https://github.com/stevearc/oil.nvim",
-	"https://github.com/shaunsingh/nord.nvim",
-	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/nvim-telescope/telescope.nvim",
 	"https://github.com/nvim-telescope/telescope-fzf-native.nvim",
 	"https://github.com/nvim-telescope/telescope-ui-select.nvim",
 	"https://github.com/folke/which-key.nvim",
-	"https://github.com/nvim-mini/mini.icons",
 }
 
                    -- Before --
